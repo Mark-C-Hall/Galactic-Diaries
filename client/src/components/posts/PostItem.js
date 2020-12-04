9870cf01e7d639d34cd1250f0084273f0639e2c8
Mark Hall <mark.hall993@gmail.com>
2020-12-03 20:40:39 -0700

    Add images to post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -5,6 +5,8 @@ import {
   Typography,
   CssBaseline,
   makeStyles,
+  Grid,
+  Avatar,
 } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
@@ -27,6 +29,7 @@ const useStyles = makeStyles((theme) => ({
 export default function PostItem(post) {
   const { title, content, author } = post.post;
   const authorName = author.name;
+  const authorThumbnail = `/photos/${author.photo}`;
   const comments = post.post.comments;
 
   const classes = useStyles();
@@ -37,9 +40,18 @@ export default function PostItem(post) {
       <Typography component="h2" variant="h3" className={classes.title}>
         {title}
       </Typography>
-      <Typography component="h3" variant="h6" className={classes.author}>
-        {authorName}
-      </Typography>
+      <Grid
+        container
+        xs={12}
+        direction="row"
+        justify="flex-start"
+        alignItems="baseline"
+      >
+        <Avatar alt={authorName} src={authorThumbnail} />
+        <Typography component="h3" variant="h6" className={classes.author}>
+          {authorName}
+        </Typography>
+      </Grid>
       <Typography component="p" variant="body1" className={classes.content}>
         {content}
       </Typography>
